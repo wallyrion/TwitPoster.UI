324b3030044e6839d3be5fc877bbf99f3b6a3d9a
wallyrion <kornienko1296@gmail.com>
2023-08-27 01:58:40 +0300

    [2.9] Infinite scroll for comments

--- src/app/components/post-comment/post-comment.component.ts
+++ src/app/components/post-comment/post-comment.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PostComment } from '../../models/comment';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post-comment',
   templateUrl: './post-comment.component.html',
   styleUrls: ['./post-comment.component.scss'],
 })
-export class PostCommentComponent {
+export class PostCommentComponent implements OnInit {
   @Input()
   public comment!: PostComment;
+  public authorImgUrl$: Observable<string | undefined> | undefined;
+
+  constructor(private readonly userService: UserService) {}
+
+  ngOnInit(): void {
+    this.authorImgUrl$ = this.userService.getProfileImage(
+      this.comment.author.id
+    );
+  }
 }
