dfd140d9eb4f256cc57d159c2ffa83b9a8059208
wallyrion <kornienko1296@gmail.com>
2023-09-27 23:34:57 +0300

    Updated author photos on post/comment

--- src/app/components/post-comment/post-comment.component.ts
+++ src/app/components/post-comment/post-comment.component.ts
@@ -1,23 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PostComment } from '../../models/comment';
-import { UserService } from '../../services/user.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post-comment',
   templateUrl: './post-comment.component.html',
   styleUrls: ['./post-comment.component.scss'],
 })
-export class PostCommentComponent implements OnInit {
+export class PostCommentComponent {
   @Input()
   public comment!: PostComment;
-  public authorImgUrl$: Observable<string | undefined> | undefined;
-
-  constructor(private readonly userService: UserService) {}
-
-  ngOnInit(): void {
-    this.authorImgUrl$ = this.userService.getProfileImage(
-      this.comment.author.id
-    );
-  }
 }
