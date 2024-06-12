6e199011a785738e7ad6c46e932745ad96a71917
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-06-12 15:41:44 +0300

    Upgrade angular to v18 (#24)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { PostCommentComponent } from './components/post-comment/post-comment.component';
 import { TwitpostsComponent } from './components/twitposts/twitposts.component';
 import { NgOptimizedImage } from '@angular/common';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -24,38 +24,32 @@ import { PostCommentsComponent } from './components/post-comments/post-comments.
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { InfiniteScrollModule } from "ngx-infinite-scroll";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    PostComponent,
-    PostCommentComponent,
-    TwitpostsComponent,
-    HeaderComponent,
-    LoginComponent,
-    FooterComponent,
-    PostCommentsComponent,
-    NotFoundComponent,
-  ],
-  imports: [
-    MatButtonModule,
-    MatInputModule,
-    BrowserModule,
-    AppRoutingModule,
-    MatCardModule,
-    BrowserAnimationsModule,
-    MatExpansionModule,
-    FormsModule,
-    MatIconModule,
-    MatBottomSheetModule,
-    NgOptimizedImage,
-    HttpClientModule,
-    MatToolbarModule,
-    ReactiveFormsModule,
-    InfiniteScrollModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        PostComponent,
+        PostCommentComponent,
+        TwitpostsComponent,
+        HeaderComponent,
+        LoginComponent,
+        FooterComponent,
+        PostCommentsComponent,
+        NotFoundComponent,
+    ],
+    bootstrap: [AppComponent], imports: [MatButtonModule,
+        MatInputModule,
+        BrowserModule,
+        AppRoutingModule,
+        MatCardModule,
+        BrowserAnimationsModule,
+        MatExpansionModule,
+        FormsModule,
+        MatIconModule,
+        MatBottomSheetModule,
+        NgOptimizedImage,
+        MatToolbarModule,
+        ReactiveFormsModule,
+        InfiniteScrollModule], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
