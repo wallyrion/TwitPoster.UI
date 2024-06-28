8f9a6fd95f84be6cce9675bdfa4d99da8af6a4bc
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-06-28 18:49:29 +0300

    Changed url for redirection (#61)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   apiUrl: 'https://twitposter-production-appservice.azurewebsites.net',
   googleClientId:
     '1043842699874-c34pau4a7j8bkhishqt1ntt3o54lk34b.apps.googleusercontent.com',
-  clientAppUrl: 'https://wallyrion.github.io/TwitPoster.UI',
+  clientAppUrl: 'https://twitposter.xyz',
 };
