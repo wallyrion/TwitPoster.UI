3012c838b88f4a09fb0b4c3513b5d2a59ddeddf5
wallyrion <kornienko1296@gmail.com>
2024-07-01 20:21:36 +0300

    About and contact components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,21 @@ import { TwitpostsComponent } from './components/twitposts/twitposts.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 const routes: Routes = [
   { path: '', component: TwitpostsComponent },
   { path: 'auth-callback', component: AuthCallbackComponent },
+  {
+    path: 'about',
+    component: AboutComponent,
+    children: [
+      { path: 'contact', component: ContactInfoComponent },
+      { path: 'technologies', component: TechnologiesComponent },
+    ],
+  },
   { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent },
 ];
---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatAnchor } from '@angular/material/button';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [RouterOutlet, MatAnchor, RouterLink],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+})
+export class AboutComponent {}
