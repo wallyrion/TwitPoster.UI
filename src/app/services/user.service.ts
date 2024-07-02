c2f37b53f56f9305e964b8d12311b9da95993301
wallyrion <kornienko1296@gmail.com>
2024-07-03 00:30:05 +0300

    Fix lint

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { apiBaseUrl } from '../core/constants/api';
 import { HttpClient } from '@angular/common/http';
 import { Account } from '../models/auth';
-import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
