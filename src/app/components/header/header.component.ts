4b2fe181765dfa9f26775afc6267481d4ad365e8
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-07-05 22:27:46 +0300

    feature/fix-flucky-google-button (#95)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CurrentUser } from '../../services/current-user.service';
 import { storageKeys } from '../../core/constants/localstorage';
-import {
-  apiBaseUrl,
-  clientAppUrl,
-  googleClientId,
-} from '../../core/constants/api';
+import { googleClientId } from '../../core/constants/api';
+import { HostListener } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Account } from '../../models/auth';
+import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+declare global {
+  const google: typeof import('google-one-tap');
+}
 
 @Component({
   selector: 'app-header',
@@ -13,19 +18,60 @@ import {
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  googleClientId = googleClientId;
-  googleSignInUrl = `${apiBaseUrl}/auth/google-sso?redirectTo=${clientAppUrl}/auth-callback`;
+  @ViewChild('google_signin_container', { static: false })
+  googleSignInContainer!: ElementRef;
 
-  constructor(public readonly currentUser: CurrentUser) {}
+  public account: Account | undefined;
+  private googleClientId = googleClientId;
+
+  constructor(
+    public readonly currentUser: CurrentUser,
+    private authService: AuthService,
+    private readonly snackBar: MatSnackBar
+  ) {}
+
+  @HostListener('window:load')
+  onLoad() {
+    google.accounts.id.initialize({
+      context: 'signin',
+      ux_mode: 'popup',
+      client_id: this.googleClientId,
+      auto_select: false,
+      itp_support: true,
+      cancel_on_tap_outside: true,
+
+      callback: a => {
+        this.authService
+          .loginWithGoogle(a.credential)
+          .pipe(switchMap(() => this.currentUser.refreshUser()))
+          .subscribe();
+      },
+    });
+
+    google.accounts.id.prompt();
+
+    google.accounts.id.renderButton(this.googleSignInContainer.nativeElement, {
+      theme: 'outline',
+      size: 'large',
+    });
+  }
 
   ngOnInit(): void {
     this.currentUser.refreshUser().subscribe(user => {
+      this.account = user;
       console.log('user successfully initialized', user);
+
+      if (user !== undefined) {
+        this.snackBar.open(`Welcome back, ${user.firstName}`, '', {
+          horizontalPosition: 'center',
+          verticalPosition: 'top',
+          duration: 2000,
+        });
+      }
     });
   }
 
   logout() {
-    this.currentUser.me = undefined;
     localStorage.setItem(storageKeys.accessTokenKey, '');
     window.location.reload();
   }
