edebb549289f6b1f86d4257a4c5045ac33a15e47
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-08-04 17:15:13 +0300

    Change api url (#126)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://twitposter-development-appservice.azurewebsites.net',
+  apiUrl: 'https://twitposter-dev1-appservice.azurewebsites.net/',
   googleClientId:
     '1043842699874-c34pau4a7j8bkhishqt1ntt3o54lk34b.apps.googleusercontent.com',
   clientAppUrl: 'https://twitposter.xyz',
