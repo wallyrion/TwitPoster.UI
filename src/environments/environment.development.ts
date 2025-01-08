6f51a8a11820f530f5bf07adbdf4904feac6e5e7
wallyrion <kornienko1296@gmail.com>
2025-01-08 22:31:49 +0100

    Use local backend for development

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: 'https://twitposter-dev1-appservice.azurewebsites.net/',
+  apiUrl: 'https://localhost:7267',
   googleClientId:
     '1043842699874-c34pau4a7j8bkhishqt1ntt3o54lk34b.apps.googleusercontent.com',
-  clientAppUrl: 'https://twitposter.site',
+  clientAppUrl: 'https://localhost:4200',
 };
