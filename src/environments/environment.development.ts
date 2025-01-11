98763d34ca9c42bc2a19d4fc2acc3e05c6700596
Oleksii Korniienko <30635131+wallyrion@users.noreply.github.com>
2025-01-11 18:28:40 +0100

    fix backend url (#234)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://localhost:7267',
+  apiUrl: 'https://twitposter-dev2-appservice.azurewebsites.net/',
   googleClientId:
     '1043842699874-c34pau4a7j8bkhishqt1ntt3o54lk34b.apps.googleusercontent.com',
   clientAppUrl: 'https://localhost:4200',
